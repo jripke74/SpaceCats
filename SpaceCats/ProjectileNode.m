41e6eb86a321e77922a52b70d2bd4076a1c70f28
Jeff Ripke <jripke74@gmail.com>
2017-07-17 11:10:54 -0700

    add animation to projectiles

--- SpaceCats/ProjectileNode.m
+++ SpaceCats/ProjectileNode.m
@@ -14,7 +14,17 @@
     ProjectileNode *projectile = [self spriteNodeWithImageNamed:@"projectile1"];
     projectile.position = position;
     projectile.name = @"Projectile";
+    [projectile setupAnimation];
     return projectile;
+}
+
+- (void) setupAnimation {
+    NSArray *texture = @[[SKTexture textureWithImageNamed:@"projectile1"],
+                         [SKTexture textureWithImageNamed:@"projectile2"],
+                         [SKTexture textureWithImageNamed:@"projectile3"]];
+    SKAction *animation = [SKAction animateWithTextures:texture timePerFrame:0.1];
+    SKAction *repeatAction = [SKAction repeatActionForever:animation];
+    [self runAction:repeatAction];
 }
 
 @end
